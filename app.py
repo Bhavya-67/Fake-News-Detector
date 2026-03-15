b922fd2ff35fbf3ecc1e71be3f439fe3d25da8f5
Bhavya-67 <cherukuribhavya216@gmail.com>
2026-03-15 11:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,19 @@
-import streamlit as st
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+import base64
 
-# ---------- Background Style ----------
-st.markdown("""
-<style>
-.stApp {
-background: linear-gradient(to right, #141e30, #243b55);
-color: white;
-}
+def set_bg():
+    with open("background.jpg","rb") as file:
+        encoded = base64.b64encode(file.read()).decode()
 
-h1 {
-text-align: center;
-color: #f5f5f5;
-}
+    page_bg = f"""
+    <style>
+    .stApp {{
+    background-image: url("data:image/jpg;base64,{encoded}");
+    background-size: cover;
+    background-position: center;
+    }}
+    </style>
+    """
 
-textarea {
-background-color: #1f2c3c !important;
-color: white !important;
-}
+    st.markdown(page_bg, unsafe_allow_html=True)
 
-.stButton>button {
-background-color: #ff4b4b;
-color: white;
-font-size: 18px;
-border-radius: 10px;
-height: 3em;
-width: 150px;
-}
-</style>
-""", unsafe_allow_html=True)
-
-# ---------- Title ----------
-st.title("📰 Fake News Detector")
-
-st.write("Paste a news article below and the AI model will predict whether it is **Real or Fake**.")
-
-# ---------- Load dataset ----------
-fake = pd.read_csv("dataset/Fake_small.csv")
-true = pd.read_csv("dataset/True_small.csv")
-
-fake["label"] = 0
-true["label"] = 1
-
-data = pd.concat([fake, true])
-
-X = data["text"]
-y = data["label"]
-
-# ---------- Train Model ----------
-vectorizer = TfidfVectorizer(stop_words="english")
-X_vector = vectorizer.fit_transform(X)
-
-model = LogisticRegression(max_iter=1000)
-model.fit(X_vector, y)
-
-# ---------- User Input ----------
-news = st.text_area("Enter News Text")
-
-if st.button("Predict"):
-
-    vec = vectorizer.transform([news])
-    prediction = model.predict(vec)
-
-    if prediction[0] == 0:
-        st.error("⚠ Fake News Detected")
-    else:
-        st.success("✅ This News Looks Real")
+set_bg()
