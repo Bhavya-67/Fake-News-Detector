522cb4d275ad71e7567edd14ce120da1e820d8cb
Bhavya-67 <cherukuribhavya216@gmail.com>
2026-03-15 09:53:17 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,14 +1,17 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-import joblib
-
 from preprocess import clean_text
 
-# Load dataset
-data = pd.read_csv("dataset/news.csv")
+# Load datasets
+fake = pd.read_csv("dataset/Fake_small.csv")
+true = pd.read_csv("dataset/True_small.csv")
+
+fake["label"] = 0
+true["label"] = 1
+
+data = pd.concat([fake,true])
 
 # Clean text
 data["text"] = data["text"].apply(clean_text)
@@ -16,28 +19,18 @@ data["text"] = data["text"].apply(clean_text)
 X = data["text"]
 y = data["label"]
 
-# TF-IDF vectorization
-vectorizer = TfidfVectorizer(max_features=5000)
+# Convert text to numbers
+vectorizer = TfidfVectorizer(stop_words="english")
 
 X_vectorized = vectorizer.fit_transform(X)
-
-# Train test split
-X_train, X_test, y_train, y_test = train_test_split(
-    X_vectorized, y, test_size=0.2, random_state=42
-)
 
 # Train model
 model = LogisticRegression()
 
-model.fit(X_train, y_train)
-
-# Prediction
-y_pred = model.predict(X_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-
-print("Model Accuracy:", accuracy)
+model.fit(X_vectorized,y)
 
 # Save model
-joblib.dump(model, "model/model.pkl")
-joblib.dump(vectorizer, "model/vectorizer.pkl")
+pickle.dump(model,open("model/model.pkl","wb"))
+pickle.dump(vectorizer,open("model/vectorizer.pkl","wb"))
+
+print("Model trained successfully")
