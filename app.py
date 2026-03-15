59a0658324404d153d8e66a7ee54d3c005dcba10
Bhavya-67 <cherukuribhavya216@gmail.com>
2026-03-15 11:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
+import streamlit as st
 import base64
 
 def set_bg():
-    with open("background.jpeg","rb") as file:
-        encoded = base64.b64encode(file.read()).decode()
+    with open("background.jpeg", "rb") as file:
+        data = file.read()
+        encoded = base64.b64encode(data).decode()
 
-    page_bg = f"""
-    <style>
-    .stApp {{
-    background-image: url("data:image/png;base64,{encoded}");
-    background-size: cover;
-    background-position: center;
-    }}
-    </style>
-    """
-
-    st.markdown(page_bg, unsafe_allow_html=True)
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpeg;base64,{encoded}");
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
 set_bg()
+
+st.title("Fake News Detector")
